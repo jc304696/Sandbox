7e753bfbc9795676c55e4bc372581b29179bad00
jc304696 <lyle.martin@my.jcu.edu.au>
2016-08-15 10:05:29 +1000

    add name printing program

--- oddName.py
+++ oddName.py
@@ -1,4 +1,14 @@
-""" This is for a git commit process"""
+""" This is for a git commit process
+    author __Lyle Martin__"""
 
-name = "Lyle"
-print(name)
+prompt = "Enter your name below"
+print(prompt)
+name = input(">>> ")
+if len(name) == 0 :
+    print('invalid name please try again')
+    name = input(">>> ")
+
+NewName = ""
+for LoopNumber in range(0,len(name),2):
+    NewName += name[LoopNumber+1]
+print(NewName)
