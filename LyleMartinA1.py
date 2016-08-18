48a51e6d84274c2b0bf52ac81afe57a059e0c8dc
jc304696 <lyle.martin@my.jcu.edu.au>
2016-08-18 13:42:19 +1000

    wrote docsting and added item.csv file

---
+++ LyleMartinA1.py
@@ -0,0 +1,14 @@
+__author__ = "Lyle Martin"
+""" Name:            Lyle Martin
+    Date:            18/08/2016
+    Program Details: The following program will be able to open a file containing a shopping list. The list will be set
+                     out as so "item name, price of the item, priority number, status(r/c)". This information will be
+                     stored and used throughout the program. The user will have the ability to:
+                     1. View a list of what is still required
+                     2. View a list that has been completed
+                     3. Add new items to the list
+                     4. Mark items as completed
+                     Once the user has finished and chooses to exit the program will save and update the file with any
+                     new items or changes to the status.
+"""
+
