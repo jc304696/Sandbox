5ea1c65db1582cc6144d3e7e7c50f1406582baef
jc304696 <lyle.martin@my.jcu.edu.au>
2016-08-24 15:42:57 +1000

    replaced dictionaries for if statements

--- Functions.py
+++ Functions.py
@@ -1,49 +1,48 @@
 import csv
 from operator import itemgetter
 
-
-def StoreFileData():
+def store_file_data():
     """Import file and save to a list.
 
     No variables to be called in. The function opens a .csv file and returns a list containing the
     content of the file.
     """
-    ItemFile = open("items.csv", "r")   # opens file
-    reader = csv.reader(ItemFile)       # saves contents of file
-    Items = []                          # creates a open list
+    item_file = open("items.csv", "r")   # opens file
+    reader = csv.reader(item_file)       # saves contents of file
+    items = []                          # creates a open list
     for line in reader:                 # cycles through each line of the stored values in reader
-        Items.append(line)              # adds to list
-    Items.sort(key=itemgetter(2))       # order the list according to priority
-    ItemFile.close()                    # closes the file
-    return Items
+        items.append(line)              # adds to list
+    items.sort(key=itemgetter(2))       # order the list according to priority
+    item_file.close()                    # closes the file
+    return items
 
-def CreateList(mainList, searchValue):
+def create_list(main_list, search_value):
     """Creates a list
 
         Uses 2 call in variables (mainList & searchValue). mainList refers to the variable that
         contains the full list. searchValue refers to the parameter that you wish to scan through
         the mainList to save to a new list. Returns the new list as its output.
     """
-    reqItems = []                               # creates an open list
-    for line in range(0, len(mainList)):        # repeats process until every line in the mainList have been searched
-        if searchValue == mainList[line][3]:    # check if searchValue is contained in the line
-            reqItems.append(mainList[line])     # saves line to reqItem list
-    reqItems.sort(key=itemgetter(2))            # sorts list by priority
-    return reqItems
+    req_items = []                               # creates an open list
+    for line in range(0, len(main_list)):        # repeats process until every line in the mainList have been searched
+        if search_value == main_list[line][3]:    # check if searchValue is contained in the line
+            req_items.append(main_list[line])     # saves line to reqItem list
+    req_items.sort(key=itemgetter(2))            # sorts list by priority
+    return req_items
 
-def UpdateFile(mainList):
+def update_file(main_list):
     """Overwrites file content
 
         Users 1 call in variable (mainList). mainList refers to the variable that contains the
         full list that you want saved to the file.
     """
-    outFile = open('newitems.csv', 'w')                 # open files and says I want to write to it
-    writer = csv.writer(outFile, lineterminator='\n')   # stores writer to outFile and end each line with an enter
-    writer.writerows(mainList)                          # tells writer where to get its information from
-    outFile.close()                                     # closes the outFile
-    print('{} items saved to items.csv'.format(len(mainList)))  # prints how many lines of information was saved
+    out_file = open('newitems.csv', 'w')                 # open files and says I want to write to it
+    writer = csv.writer(out_file, lineterminator='\n')   # stores writer to outFile and end each line with an enter
+    writer.writerows(main_list)                          # tells writer where to get its information from
+    out_file.close()                                     # closes the outFile
+    print('{} items saved to items.csv'.format(len(main_list)))  # prints how many lines of information was saved
 
-def PrintList(workingList):
+def print_list(working_list):
     """Prints a list
 
         Users 1 call in variable (woorkingList). workingList refers to the list that you want printed.
@@ -52,41 +51,81 @@ def PrintList(workingList):
         the required criteria you can print it to the screen with this function.
     """
     TotalCost = 0                                   # creates variable
-    for Item in range(0, len(workingList)):         # repeats process until every list value has been printed
-        TotalCost += float(workingList[Item][1])    # adds 'price' parameter to variable
-        print("{0:d}. {1:25s} ${2:>8.2f} ({3})".format(Item, workingList[Item][0], float(workingList[Item][1]),
-                                                       workingList[Item][2]))   # prints list information in a perticular format
+    for Item in range(0, len(working_list)):         # repeats process until every list value has been printed
+        TotalCost += float(working_list[Item][1])    # adds 'price' parameter to variable
+        print("{0:d}. {1:25s} ${2:>8.2f} ({3})".format(Item, working_list[Item][0], float(working_list[Item][1]),
+                                                       working_list[Item][2]))   # prints list information in a perticular format
+    print('Total expected price for {0} items: ${1:.2f}\n'.format(len(working_list), TotalCost)) # prints total price of all items
 
-    print('Total expected price for {0} items: ${1:.2f}\n'.format(len(workingList), TotalCost)) # prints total price of all items
-
-def MarkComplete(mainList, workingList):
+def mark_complete(main_list, working_list):
     """Changing parameter in list
 
         Users 2 call in variables (mainList & workingList). mainList refers to the variable that contains
         the full list. workingList refers to the variable that contains a list with certain criteria (i.e.
         this list usually equal to or smaller than the mainList)
     """
-    PrintList(workingList)                                      # user the PrintList() function to print the workingList
+    print_list(working_list)                                      # user the PrintList() function to print the workingList
     print('Enter the number of an item to mark as completed')   # prints instructions
-
-    while True:                                                 # creates an infinite loop
-        try:                                                    # does no matter what
-            ItemNumber = int(input('>>> '))                     # gets user input
-            if ItemNumber > (len(workingList) - 1):             # checks the input against certain criteria
+    while True:                                             # creates an infinite loop
+        try:                                                # does no matter what
+            item_number = int(input('>>> '))                 # gets user input
+            if item_number > (len(working_list) - 1):         # checks the input against certain criteria
                 print('Invalid item number')
-            elif ItemNumber < 0:                                # checks the input against certain criteria
+            elif item_number < 0:                            # checks the input against certain criteria
                 print('Invalid item number')
-            else:                                               # if the earlier checks where False then does the following
-                break                                           # breaks out of the infinite loop
-        except ValueError:                                      # catches error response
+            else:                                           # if the earlier checks return False then does the following
+                break                                       # breaks out of the infinite loop
+        except ValueError:                                  # catches error response
             print('Invalid input; enter a number')
 
+    item = working_list[item_number]                          # stores the value from the list to a new variable
+    for line in range(0, len(main_list)):
+        if item[0] == main_list[line][0]:                    # checks if the value in item matches any value in mainList
+            main_list[line][3] = 'c'                         # changes a value in mainList
 
-    item = workingList[ItemNumber]
+    print("{0} marked as completed\n".format(item[0]))      # tells user what was changed
+    return main_list
 
-    for line in range(0, len(mainList)):
-        if item[0] == mainList[line][0]:
-            mainList[line][3] = 'c'
+def add_item(main_list):
+    """Adding a list to a list
 
-    print("{0} marked as completed\n".format(item[0]))  # tells user what was changed
-    return mainList
+        Asks the user for certain information (item name, item price & priority) and saves
+        it to a list (newList). Then that list is saved to the mainList (ITEMLIST).
+    """
+
+    while True:
+        try:
+            name = str(input('Item name: '))
+            if len(name) == 0:
+                print('Input can not be blank')
+            else:
+                break
+        except Exception:
+            print('Invalid input try again')
+
+    while True:
+        try:
+            price = float(input('Price: $ '))
+            if price < 0:
+                print('Price must be >= $0')
+            else:
+                break
+        except ValueError:
+            print('Invalid input; enter a valid number')
+
+    while True:
+        try:
+            priority = int(input('Priority: '))
+            if priority <= 0:
+                print('Priority must be 1, 2 or 3')
+            elif priority > 3:
+                print('Priority must be 1, 2 or 3')
+            else:
+                break
+        except ValueError:
+            print('Invalid input; enter a valid number')
+
+    new_item = [name, str(price), str(priority), 'r']
+    main_list.append(new_item)
+    main_list.sort(key=itemgetter(2))
+    print('{}, ${:.2f} (priority {}) added to shopping list\n'.format(name, float(price), priority))
--- LyleMartinA1.py
+++ LyleMartinA1.py
@@ -2,109 +2,57 @@ __author__ = "Lyle Martin"
 """ Name:            Lyle Martin
     Date:            18/08/2016
     Program Details: The following program will be able to open a file containing a shopping list. The list will be set
-                     out as so "item name, price of the item, priority number, status(r/c)". This information will be
+                     out as so "item name, item price, priority number, status(r/c)". This information will be
                      stored and used throughout the program. The user will have the ability to:
-                     1. View a list of what is still required
-                     2. View a list that has been completed
+                     1. View a list required list
+                     2. View a list completed list
                      3. Add new items to the list
                      4. Mark items as completed
-                     Once the user has finished and chooses to exit the program will save and update the file with any
-                     new items or changes to the status.
+                     Once the user has finished and chooses to exit the program, the list will be updated in the original
+                     file.
 """
-
-
-from operator import itemgetter
-from Functions import PrintList, StoreFileData, UpdateFile, CreateList, MarkComplete
-
-InFile = open("items.csv", "r")
-print(InFile.read())
-InFile.close()
-ITEMLIST = StoreFileData()
-MENU = "Menu:\nR - List required items\nC - List completed items\nA - Add new item\nM - Mark item as completed\nQ - Quit"
-
-print('Shopping List 1.0 - by Lyle Martin')
-print("{} items loaded from items.csv".format(len(ITEMLIST)))
+from Functions import print_list, store_file_data, update_file, create_list, mark_complete, add_item
 
 def main():
-    options = {'R' : caseR, 'C' : caseC, 'A' : AddItem, 'M' : caseM}
-    print(MENU)
+    in_file = open("items.csv", "r")
+    print(in_file.read())
+    in_file.close()
+    item_list = store_file_data()
+    menu = "Menu:\nR - List required items\nC - List completed items\nA - Add new item\nM - Mark item as completed\nQ - Quit"
+
+    print('Shopping List 1.0 - by Lyle Martin')
+    print("{} items loaded from items.csv".format(len(item_list)))
+    print(menu)
     choice = input(">>> ").upper()
 
     while choice != 'Q':
-        try: options[choice]()
-        except KeyError:
-            print("Invalid menu choice\n")
-        print(MENU)
+        if choice == 'R':
+            req_list = create_list(item_list, 'r')
+            if len(req_list) == 0:
+                print('No required items')
+            else:
+                print('Required items:')
+                print_list(req_list)
+        elif choice == 'C':
+            com_list = create_list(item_list, 'c')
+            if len(com_list) == 0:
+                print('No completed items')
+            else:
+                print('Completed Items:')
+                print_list(com_list)
+        elif choice == 'A':
+            add_item(item_list)
+        elif choice == 'M':
+            req_list = create_list(item_list, 'r')
+            if len(req_list) == 0:
+                print('No required items\n')
+            else:
+                mark_complete(item_list, req_list)
+
+        print(menu)
         choice = input(">>> ").upper()
 
-    UpdateFile(ITEMLIST)
+    update_file(item_list)
     print('Have a nice day :)')
-
-def caseR():
-    reqList = CreateList(ITEMLIST, 'r')
-    if len(reqList) == 0:
-        print('No required items')
-    else:
-        print('Required items:')
-        PrintList(reqList)
-
-def caseC():
-    comList = CreateList(ITEMLIST, 'c')
-    if len(comList) == 0:
-        print('No completed items')
-    else:
-        print('Completed Items:')
-        PrintList(comList)
-
-def AddItem():
-    """Adding a list to a list
-
-        Asks the user for certain information and saves it to a list (newList). Then that list is
-        saved to the mainList (ITEMLIST).
-    """
-
-    while True:
-        try:
-            name = str(input('Item name: '))
-            if len(name) == 0:
-                print('Input can not be blank')
-            else:
-                break
-        except Exception:
-            print('Invalid input try again')
-
-    while True:
-        try:
-            price = float(input('Price: $ '))
-            if price < 0:
-                print('Price must be >= $0')
-            else:
-                break
-        except ValueError:
-            print('Invalid input; enter a valid number')
-
-    while True:
-        try:
-            priority = int(input('Priority: '))
-            if priority <= 0:
-                print('Priority must be 1, 2 or 3')
-            elif priority > 3:
-                print('Priority must be 1, 2 or 3')
-            else:
-                break
-        except ValueError:
-            print('Invalid input; enter a valid number')
-
-    NewItem = [name, str(price), str(priority), 'r']
-    ITEMLIST.append(NewItem)
-    ITEMLIST.sort(key=itemgetter(2))
-    print('{}, ${:.2f} (priority {}) added to shopping list\n'.format(name, float(price), priority))
-
-def caseM():
-    reqList = CreateList(ITEMLIST, 'r')
-    if len(reqList) == 0:
-        print('No required items\n')
-    else:
-        MarkComplete(ITEMLIST, reqList)
 
 main()
